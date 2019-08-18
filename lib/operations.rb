a9b1f1533cb2d9b5a0ff7af8713d063a6885871a
Panela Leung <leungpanela@gmail.com>
2019-08-18 09:28:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,13 +12,13 @@ def not_safe?(speed)
   #if speed > 60 && speed < 40
     #true
   #end
-  speed > 60? (speed < 40? true:false): false
+  #speed > 60? (speed < 40? true:false): false
   if speed > 60
     true
   elsif speed < 40
     true
   elsif
-
   false
 end
+  speed > 60? (speed < 40? true:false): false
 end
